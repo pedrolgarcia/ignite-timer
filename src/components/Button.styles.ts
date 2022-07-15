bdc2e59c39c74ff1dff2816868942328e3e2f1b1
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-07-14 23:08:33 -0300

    fix: remove button variants

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -6,21 +6,9 @@ interface ButtonContainerProps {
   variant: ButtonVariant
 }
 
-const buttonVariants = {
-  primary: 'purple',
-  secondary: 'orange',
-  danger: 'red',
-  success: 'green',
-}
-
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
 
   background-color: ${({ theme }) => theme['green-500']};
-
-  /* ${({ variant }) =>
-    css`
-      background-color: ${buttonVariants[variant]};
-    `} */
 `
