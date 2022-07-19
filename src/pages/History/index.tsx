5cf491c6066dac42ed17c3666fc51863bd93c3b3
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-07-18 23:45:55 -0300

    feat: start history page

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,13 @@
+import { HistoryContainer } from './styles'
+
+function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <div></div>
+    </HistoryContainer>
+  )
+}
+
+export default History
